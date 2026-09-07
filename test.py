9135b4a975851a4d9c503690948f88680b1644d6
Thrivenimuthyala <thrivenimuthyala03@gmail.com>
2026-09-07 12:54:38 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+n = int(input())
+intervals = []
+for _ in range(n):
+    start, end = map(int, input().split())
+    intervals.append([start, end])
+intervals.sort()
+merged = []
+for start, end in intervals:
+    if not merged or start > merged[-1][1]:
+        merged.append([start, end])
+    else:
+        merged[-1][1] = max(merged[-1][1], end)
+for start, end in merged:
+    print(start, end)
